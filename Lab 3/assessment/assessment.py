7acd9b3e2f8b335b05b0e6e0ee01294ec1d0426d
Kirill Setdekov <ksetdekov@gmail.com>
2022-01-23 16:11:38 +0300

    chagne batch size to 64

--- Lab 3/assessment/assessment.py
+++ Lab 3/assessment/assessment.py
@@ -30,7 +30,7 @@ if gpus:
 
 parser = argparse.ArgumentParser(description='CIFAR-10 Example',
                                  formatter_class=argparse.ArgumentDefaultsHelpFormatter)
-parser.add_argument('--batch-size', type=int, default=256,
+parser.add_argument('--batch-size', type=int, default=64,
                     help='input batch size for training')
 parser.add_argument('--epochs', type=int, default=50,
                     help='number of epochs to train')
